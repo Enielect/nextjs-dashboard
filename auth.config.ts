e3555800b0223dee2d167b245eac738ecebb28f1
eniola <eniolafaithful57@gmail.com>
2024-04-29 21:19:36 +0100

    trying everything to debug this failng redirection after login confirmed

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,5 @@
 import type { NextAuthConfig } from 'next-auth';
- 
+
 export const authConfig = {
   pages: {
     signIn: '/login',
@@ -7,12 +7,14 @@ export const authConfig = {
   //Next, add the logic to protect your routes. This will prevent users from accessing the dashboard pages unless they are logged in.
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
+      console.log('processing...');
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
+        console.log('logged-in');
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
