0bf79eac2c77de0c6b5d251340cc068162ade402
eniola <eniolafaithful57@gmail.com>
2024-04-30 18:02:46 +0100

    consoling to find out the problem with authentication

--- auth.ts
+++ auth.ts
@@ -2,13 +2,14 @@ import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
-import type { User } from './app/lib/definitions';
+import type { User } from './app/lib/definitions';  
 import { sql } from '@vercel/postgres';
 import bcrypt from 'bcrypt';
 
-async function getUser(email: string): Promise<User | undefined> {
+export async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    console.log('user gotten successfully');
     return user.rows[0];
   } catch (error) {
     console.error('Failed to fetch user:', error);
